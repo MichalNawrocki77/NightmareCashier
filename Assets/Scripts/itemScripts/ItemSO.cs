c576afd46bc1c9bbda08a086ae6230860a10be4a
Michał Nawrocki <m.nawrocki77@o2.pl>
2023-11-29 12:48:57 +0100

    Zaczeto szkielet abstrakcyjnej klasy Item (Raport z zajec 29.11)

---
+++ Assets/Scripts/itemScripts/ItemSO.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class ItemSO : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    Sprite spriteInInventory;
+    SpriteRenderer spriteRenderer;
+    //przekminie pozniej co to za warning mi wylatuje
+    Collider2D collider;
+    Vector3 initialPositionOnBeginDrag;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        initialPositionOnBeginDrag = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        eventData.pointerCurrentRaycast.gameObject
+        
+    }
+    
+}
